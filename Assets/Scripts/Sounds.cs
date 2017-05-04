d91080fdfb96a12e0970ef6bc51045b9f0ccd707
Orri Axelsson <orriaxels@gmail.com>
2017-05-04 10:33:51 +0000

    sound script added

---
+++ Assets/Scripts/Sounds.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds : MonoBehaviour {
+
+	public AudioSource SoundSource;
+	public AudioClip Sound;
+
+	float incVolume = 0.0f;
+	float volRate = 0.1f;
+
+	private bool hasPlayedAudio;
+
+	void OnTriggerEnter(Collider other)
+	{
+		
+		if (other.gameObject.CompareTag ("Player") && hasPlayedAudio == false) 
+		{
+			SoundSource.clip = Sound;
+			SoundSource.Play ();
+			hasPlayedAudio = true;
+
+		} 
+		else if (other.gameObject.CompareTag ("Player") && hasPlayedAudio == true) 
+		{
+			SoundSource.Stop ();
+			hasPlayedAudio = false;
+		}
+	}
+
+	void increaseVol() 
+	{
+		while (incVolume < 1.0) {
+			incVolume += incVolume * (Time.deltaTime);
+		}
+	}
+}
