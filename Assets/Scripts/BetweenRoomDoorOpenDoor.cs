fc8707cc3c5bf35634e955f067ccf911b2b43305
HaraldurShoshan <haraldurshoshan@gmail.com>
2017-05-04 12:41:42 +0000

    roomBetween fix

---
+++ Assets/Scripts/BetweenRoomDoorOpenDoor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetweenRoomDoorOpenDoor : MonoBehaviour {
+
+	public AudioClip closeDoor;
+	AudioSource audio;
+	public float Volume;
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+		Volume = 1f;
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		audio.PlayOneShot (closeDoor, Volume);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
