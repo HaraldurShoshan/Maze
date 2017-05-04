88a67e534e28d2b65a3009847f8b2826171ca66d
HaraldurShoshan <haraldurshoshan@gmail.com>
2017-05-04 11:30:59 +0000

    First Room Finished

---
+++ Assets/Scripts/DoorSoundOpen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSoundOpen : MonoBehaviour {
+	public AudioClip openDoor;
+	AudioSource audio;
+	public float Volume;
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+		Volume = 1f;
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		audio.PlayOneShot (openDoor, Volume);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/LeverController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverController : MonoBehaviour {
+
+	private Animator anim = null;
+	private Animator doorAnim = null;
+	private bool lever;
+	private bool openDoor;
+	private GameObject mainDoor;
+	// Use this for initialization
+	void Start () {
+		lever = false;
+		openDoor = false;
+		anim = GetComponent<Animator> ();
+		mainDoor = GameObject.Find ("Door");
+		doorAnim = mainDoor.GetComponent<Animator> ();
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		lever = true;
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		lever = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("f") && lever && !openDoor) {
+			Debug.Log ("if");
+			openDoor = true;
+			anim.SetBool ("open", true);
+			doorAnim.SetBool ("isOpen", true);
+
+		} else if (Input.GetKeyDown ("f") && lever && openDoor) 
+		{
+			Debug.Log ("else");
+			openDoor = false;
+			anim.SetBool ("open", false);
+			doorAnim.SetBool ("isOpen", false);
+		}
+	}
+}
