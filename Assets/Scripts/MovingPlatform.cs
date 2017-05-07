7013e4fbb0a4c47fc7f5a00e55352c7699235f8c
Orri Axelsson <orriaxels@gmail.com>
2017-05-07 10:12:05 +0000

    drowning sound added

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour {
 
-	public float speed = 0.005f;
+	public AudioClip drowning;
+	AudioSource SoundSource;
+	public float volume;
+
+	public float speed;
 	private bool inMaze = false;
 	private bool alive = true;
 	private bool enteredMaze = false;
+
+	void Start(){
+		SoundSource = GetComponent<AudioSource> ();
+		volume = 0.4f;
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		Debug.Log ("Am I in the maze? " + inMaze);
 		Debug.Log ("Am I alive? " + alive);
 		Debug.Log ("Have I entered the maze? " + enteredMaze);
-		if(inMaze && alive)
+		if (inMaze && alive) 
+		{
+			speed = 0.05f;
 			transform.Translate (Vector3.up * speed * 1 * Time.deltaTime);
-		if(!inMaze && alive && enteredMaze)
+		}
+
+		if (!inMaze && alive && enteredMaze) 
+		{
+			speed = 0.2f;
 			transform.Translate (Vector3.up * speed * -1 * Time.deltaTime);
+		}
 	}
 
 	void OnTriggerEnter(Collider other)
@@ -31,10 +47,11 @@ public class MovingPlatform : MonoBehaviour {
 	
 		if (other.tag == "UpperLimit") 
 		{
+			SoundSource.PlayOneShot (drowning, volume);
 			alive = false;
 		} 
 		else if (other.tag == "LowerLimit") 
-		{		
+		{	
 			enteredMaze = false;
 		}
 		
