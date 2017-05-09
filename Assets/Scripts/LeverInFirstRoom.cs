9c2a36fdea7affe7f372527f22d912ea51d0fecc
HaraldurShoshan <haraldurshoshan@gmail.com>
2017-05-09 17:07:24 +0000

    TutorialMazeFix

--- Assets/Scripts/LeverInFirstRoom.cs
+++ Assets/Scripts/LeverInFirstRoom.cs
@@ -31,7 +31,7 @@ public class LeverInFirstRoom : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (mission.missionAccomplished) {
+		if (!mission.missionAccomplished) {
 		
 			Debug.Log ("Now you can Open the door");
 		
