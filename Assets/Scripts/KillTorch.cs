00ad301e4676bdbd542db9806631f9a9412d8275
Orri Axelsson <orriaxels@gmail.com>
2017-05-09 11:22:56 +0000

    firefix

---
+++ Assets/Scripts/KillTorch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillTorch : MonoBehaviour {
+
+	public bool fireOff;
+
+	// Use this for initialization
+	void Start () {
+		fireOff = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (fireOff) {
+			gameObject.SetActive (false);
+		}
+	}
+
+	void OnTriggerEnter (Collider other){
+		if (other.gameObject.CompareTag ("Water")) {
+			fireOff = true;
+		}
+	}
+}
