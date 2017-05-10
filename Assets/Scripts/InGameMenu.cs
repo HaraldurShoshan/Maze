a2268f68d61f736d711dca4b56ebf0064a752724
HaraldurShoshan <haraldurshoshan@gmail.com>
2017-05-10 14:57:02 +0000

    star scene menu and pause menu

---
+++ Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour {
+	public Canvas pauseMenu;
+	public Button resumeText;
+	public Button quitText;
+	bool paused;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.visible = false;
+		pauseMenu.enabled = false;
+		pauseMenu = pauseMenu.GetComponent<Canvas> ();
+		resumeText = resumeText.GetComponent<Button>();
+		quitText = quitText.GetComponent<Button>();
+
+	}
+
+	public void StartLevel(){
+		paused = false;
+		pauseMenu.enabled = false;
+	}
+
+	public void ExitGame(){
+		paused = false;
+		SceneManager.LoadScene (0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape) && !paused) {
+			pauseMenu.enabled = true;
+			paused = true;
+		} else if (Input.GetKeyDown (KeyCode.Escape) && paused) {
+			pauseMenu.enabled = false;
+			paused = false;
+		}
+	}
+}
--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -6,21 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour {
 
-	public Canvas quitMenu;
 	public Button startText;
 	public Button quitText;
 
 
 	// Use this for initialization
 	void Start () {
-		quitMenu = quitMenu.GetComponent<Canvas> ();
+		Cursor.visible = false;
 		startText = startText.GetComponent<Button> ();
 		quitText = quitText.GetComponent<Button> ();
-		quitMenu.enabled = false;
 	}
 
 	public void ExitPress(){
-		quitMenu.enabled = true;
 		startText.enabled = false;
 		quitText.enabled = false;
 		startText.gameObject.SetActive (false);
@@ -28,7 +25,6 @@ public class MenuScript : MonoBehaviour {
 	}
 
 	public void NoPress(){
-		quitMenu.enabled = false;
 		startText.enabled = true;
 		quitText.enabled = true;
 		startText.gameObject.SetActive (true);
