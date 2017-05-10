e3d8e5abb5d33f2a2766ced16796132ebaf85e37
Orri Axelsson <orriaxels@gmail.com>
2017-05-10 22:20:41 +0000

    make the player walk slower if water reached certain height

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class MovingPlatform : MonoBehaviour {
 
@@ -19,23 +20,25 @@ public class MovingPlatform : MonoBehaviour {
 	public bool stop = false;
 
 //	Vector3 SpawnPoint;
-//	GameObject player;
+	FirstPersonController player;
+	InMazeLevers lev;
 
 	void Start(){
 		SoundSource = GetComponent<AudioSource> ();
-//		player = GameObject.FindWithTag("Player");
+		player = GameObject.Find ("FPSController").GetComponent<FirstPersonController>();
 //		SpawnPoint = new Vector3(-5f, 0, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		
 		//Debug.Log ("Pulled " + pulledLever);
 		//Debug.Log ("Am I in the maze? " + inMaze);
 		//Debug.Log ("Am I alive? " + alive);
 		//Debug.Log ("Have I entered the maze? " + enteredMaze);
 		//Debug.Log ("Down ? " + moveDown);
 		//Debug.Log("Slower? " + slower);
+
 
 
 		if (inMaze && alive && !pulledLever) 
@@ -51,6 +54,9 @@ public class MovingPlatform : MonoBehaviour {
 			speed = 0.2f;
 			transform.Translate (Vector3.up * speed * -1 * Time.deltaTime);
 		}
+
+
+
 	}
 
 	void OnTriggerEnter(Collider other)
@@ -65,7 +71,7 @@ public class MovingPlatform : MonoBehaviour {
 
 		if(other.tag == "Slower")
 		{
-			slower = true;
+			player.m_WalkSpeed = 3.5f;
 		}
 
 		if (other.tag == "UpperLimit") 
@@ -96,7 +102,8 @@ public class MovingPlatform : MonoBehaviour {
 
 		if(other.tag == "Slower")
 		{
-			slower = false;
+			player.m_WalkSpeed = 5.0f;
+
 		}
 	
 	}		
