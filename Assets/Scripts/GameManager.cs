a8c4af6495e6284331c7d6b7ae931ea3348c2bb8
HaraldurShoshan <haraldurshoshan@gmail.com>
2017-05-11 14:47:08 +0000

    gameController added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public int LevelCounter;
+	public GameObject lf_one;
+	public GameObject lf_two;
+	public GameObject lf_three;
+	private bool lfFirst;
+	private bool lfSecond;
+	private bool lfThird;
+	// Use this for initialization
+	void Start () {
+		LevelCounter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (lf_one.GetComponent<LeverFinished> ().Levelfinished && !lfFirst) {
+			LevelCounter++;
+			lfFirst = true;
+		}
+		if (lf_two.GetComponent<LeverFinished> ().Levelfinished && !lfSecond) {
+			LevelCounter++;
+			lfSecond = true;
+		}
+		if (lf_three.GetComponent<LeverFinished> ().Levelfinished && !lfThird) {
+			LevelCounter++;
+			lfThird = true;
+		}
+
+
+		Debug.Log (LevelCounter);
+	}
+}
