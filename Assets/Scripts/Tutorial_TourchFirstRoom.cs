abf3f5b0d6f5109e4ab7384a1a6296be92b505b6
HaraldurShoshan <haraldurshoshan@gmail.com>
2017-05-12 11:11:53 +0000

    Final scene cleanup

---
+++ Assets/Scripts/Tutorial_TourchFirstRoom.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial_TourchFirstRoom : MonoBehaviour {
+
+	private Canvas myCanvas;
+	public bool display;
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Testing");
+		myCanvas = GetComponent<Canvas>();	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (display) {
+			Debug.Log ("on");
+			myCanvas.enabled = true;
+		} else if (!display) {
+			myCanvas.enabled = false;
+		}
+	}
+}
--- Assets/Scripts/tutorialBox.cs
+++ Assets/Scripts/tutorialBox.cs
@@ -19,5 +19,6 @@ public class tutorialBox : MonoBehaviour {
 			tt.display = true;
 			display = true;
 		}
+
 	}
 }
