836939c6733a4a484965b2b8745c5a61be80b962
HaraldurShoshan <haraldurshoshan@gmail.com>
2017-05-13 15:19:55 +0000

    intro fix

--- Assets/Scripts/IntroFade.cs
+++ Assets/Scripts/IntroFade.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class IntroFade : MonoBehaviour {
 
 	public Canvas fade;
 	CanvasGroup fadeAlpha;
 	private bool hit;
+	private bool load;
 
-	public Camera cam;
 	// Use this for initialization
 	void Start () {
 		Debug.Log ("test");	
@@ -19,12 +20,18 @@ public class IntroFade : MonoBehaviour {
 		if (other.CompareTag("Torch")) {
 			hit = true;
 		}
+		if (other.CompareTag ("Spawn")) {
+			load = true;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (hit) {
 			fadeAlpha.alpha += (0.6f * Time.deltaTime);
+		}
+		if (load) {
+			SceneManager.LoadScene (2);
 		}
 	}
 
