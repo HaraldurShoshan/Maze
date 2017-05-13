43c265bda55e94f0e9ca84635e2b36fe00cb87a0
HaraldurShoshan <haraldurshoshan@gmail.com>
2017-05-13 17:30:12 +0000

    final fix

---
+++ Assets/Scripts/ExitLight.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitLight : MonoBehaviour {
+	public bool hit;
+	public bool unHit;
+	Light light;
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light> ();
+	}
+
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.CompareTag("Player")){
+			hit = true;
+			unHit = false;
+		}
+	}
+
+	void OnTriggerExit(Collider col){
+		if (col.gameObject.CompareTag ("Player")) {
+			unHit = true;
+			hit = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hit) {
+			light.intensity += (5f * Time.deltaTime);
+		}
+		if (unHit) {
+			if (light.intensity > 0f) {
+				light.intensity -= (5f * Time.deltaTime);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/FinishCollide.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishCollide : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.CompareTag ("Player")) {
+			SceneManager.LoadScene (0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
